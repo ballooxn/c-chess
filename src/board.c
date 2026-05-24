f32c457722a0c187fcdb4eeabf8bb40ba927d332
Lincoln Digby <lincolndigby0@gmail.com>
2026-05-24 18:25:53 -0500

    Start working on player input

--- src/board.c
+++ src/board.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 // bb = (black top, white bottom)
 // a8 b8 c8 d8 e8 f8 g8 h8 > 56 to 63
@@ -37,6 +38,24 @@ void print_bitboard(uint64_t board) {
     printf("    a b c d e f g h\n");
 }
 
+// input will be like "pe2e4" (piece, starting pos, ending pos)
+// make it more explanatory in the future (simple for me rn)
+char* player_input(char* buffer, size_t size) {
+    if (buffer == NULL || size == 0) {
+        return NULL;
+    }
+
+    if (fgets(buffer, size, stdin) == NULL) {
+        buffer[0] = '\0';
+        return NULL;
+    }
+
+    // remove potential newline
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+    return buffer;
+}
+
 int main(void) {
 
     Board board = {0};
@@ -45,5 +64,9 @@ int main(void) {
 
     printf("This is the white_pieces in decimal: %lu\n", board.white_pieces);
     print_bitboard(board.white_pieces);
+    puts("Please choose a starting location");
+    char start[3];
+    player_input(start, sizeof(start));
+    printf("%s\n", start);
     return 0;
 }
